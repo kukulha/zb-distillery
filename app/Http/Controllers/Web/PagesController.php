f54630d6528dd98808c2a387773dfdefd431afe7
Emmanuel Valenzuela <emmanuel@kukulha.com>
2019-03-13 14:01:18 -0600

    Se arreglo el cacheo en los posts

--- app/Http/Controllers/Web/PagesController.php
+++ app/Http/Controllers/Web/PagesController.php
@@ -25,32 +25,40 @@ class PagesController extends Controller
     {
         $key =  "posts" ;
 
-        $posts = Cache::rememberForever($key, now()->addMinutes(10), function(){
+        $posts = Cache::remember($key, now()->addMinutes(10), function(){
             return Post::with(['user'])->orderBy('id', 'DESC')->where('status', 'PUBLISHED')->paginate(3);
         });
     	return view('index', compact('posts'));
     }
+    
     public function zb()
     {
     	return view('web.zb');
     }
+
     public function blog()
     {
-        $key =  "posts.page." . request('page', 1);
+        $key =  "posts.blog." . request('page', 1);
 
         $posts = Cache::remember($key, now()->addMinutes(10), function(){
             return Post::with(['user'])->orderBy('id', 'DESC')->where('status', 'PUBLISHED')->paginate(12);
         });
+
     	return view('web.posts', compact('posts'));
     }
+
     public function post($slug)
-    {
-    	$post = Post::where('slug', $slug)->first();
-    	$related= Post::where('category_id', $post->category->id)
+    {   
+        $post = Cache::remember("blog.{$slug}", now()->addMinutes(10), function() use($slug){
+            return Post::where('slug', $slug)->first();
+        } );
+    	$related= Post::inRandomOrder()
+            ->where('category_id', $post->category->id)
             ->where('id', '!=', $post->id)
             ->paginate(3);
     	return view('web.post', compact('post', 'related'));
     }
+
     public function admin()
     {
         $categories = Category::orderBy('id', 'DESC')->paginate(5);
